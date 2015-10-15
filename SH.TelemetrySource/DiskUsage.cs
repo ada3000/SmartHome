98fa507c1c6bb1cfb9463e791f77ce733db7300e
ADA <ada3000@gmail.com>
2015-10-15 16:09:46 +0300

    add disk usage

---
+++ SH.TelemetrySource/DiskUsage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SH.BO;
+
+namespace SH.TelemetrySource
+{
+	public class DiskUsage: ISensorValueSource
+	{
+		public IEnumerable<SensorValue> Collect()
+		{
+			List<SensorValue> result = new List<SensorValue>();
+
+			foreach (System.IO.DriveInfo di in System.IO.DriveInfo.GetDrives())
+			{
+					SensorValue drive = new SensorValue
+					{
+						Date = DateTime.UtcNow,
+						Children = new List<SensorValue>(),
+						Name = di.VolumeLabel + " (" + di.Name.Replace("\\","") + ")",
+						Description = "Disk persent usage",
+						Type = SensorValueType.Hdd,
+						Value = 100 - 100 * di.AvailableFreeSpace / di.TotalSize,
+						WarningValueMin = 90, //warning when 90% is used,
+						ValueScale = SensorValueScale.Persent,
+						ValueMin = 0,
+						ValueMax = 100
+					};
+
+					drive.Children.Add(new SensorValue
+						{
+							Type = SensorValueType.Hdd,
+							SubType = "AvailableFreeSpace",							
+							ValueScale = SensorValueScale.Byte,
+							Value = di.AvailableFreeSpace
+						});
+					drive.Children.Add(new SensorValue
+					{
+						Type = SensorValueType.Hdd,
+						SubType = "TotalSize",						
+						ValueScale = SensorValueScale.Byte,
+						Value = di.TotalSize
+					});
+
+					result.Add(drive);
+			}
+
+			return result;
+		}
+	}
+}
